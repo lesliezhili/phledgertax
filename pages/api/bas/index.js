8a862e6c45feb96a151d79a9b49014ed6cc0a644
lesliezhili <zhili@phledger.com>
2026-05-25 18:07:19 +1000

    fix(api): use @/ alias for lib imports in index.js

--- pages/api/bas/index.js
+++ pages/api/bas/index.js
@@ -1,6 +1,6 @@
-import { loadCountry, AU_BANKS } from '../../../lib/store.js';
-import { autoCategorise } from '../../../lib/categoriser.js';
-import { generateBasDraft } from '../../../lib/basAu.js';
+import { loadCountry, AU_BANKS } from '@/lib/store.js';
+import { autoCategorise } from '@/lib/categoriser.js';
+import { generateBasDraft } from '@/lib/basAu.js';
 export default function handler(req, res) {
   const txs = autoCategorise(loadCountry('AU'));
   const { start, end } = req.query;
