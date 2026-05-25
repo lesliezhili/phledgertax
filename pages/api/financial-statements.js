c8f4d135b9e7de62e539b9ddcab62bb60ac7de44
lesliezhili <zhili@phledger.com>
2026-05-25 18:07:29 +1000

    fix(api): use @/ alias for lib imports in financial-statements.js

--- pages/api/financial-statements.js
+++ pages/api/financial-statements.js
@@ -1,6 +1,6 @@
-import { loadAll, loadCountry, AU_BANKS, CA_BANKS } from '../../../lib/store.js';
-import { autoCategorise } from '../../../lib/categoriser.js';
-import { generateFinancialStatements } from '../../../lib/financialStatements.js';
+import { loadAll, loadCountry, AU_BANKS, CA_BANKS } from '@/lib/store.js';
+import { autoCategorise } from '@/lib/categoriser.js';
+import { generateFinancialStatements } from '@/lib/financialStatements.js';
 export default function handler(req, res) {
   const { country, as_of } = req.query;
   let txs = country ? loadCountry(country) : loadAll();
