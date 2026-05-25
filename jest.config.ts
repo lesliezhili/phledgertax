5110d077416a3a72c6a08d9efb7b6fd4bf89c685
lesliezhili <zhili@phledger.com>
2026-05-25 17:56:25 +1000

    fix(jest): fix setupFilesAfterAllFrameworks typo → setupFilesAfterFramework

--- jest.config.ts
+++ jest.config.ts
@@ -6,7 +6,7 @@ const createJestConfig = nextJest({ dir: './' });
 const config: Config = {
   coverageProvider: 'v8',
   testEnvironment: 'node',
-  setupFilesAfterAllFrameworks: [],
+  setupFilesAfterFramework: ['<rootDir>/jest.setup.ts'],
   moduleNameMapper: { '^@/(.*)$': '<rootDir>/$1' },
   testMatch: ['<rootDir>/__tests__/**/*.[jt]s?(x)'],
   collectCoverageFrom: [
